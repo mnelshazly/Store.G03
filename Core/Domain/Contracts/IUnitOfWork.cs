cd951d33c3232cb7c089f17dc36bb76f4f18ed95
mnelshazly <mnelshazly7@gmail.com>
2025-04-11 17:02:35 +0200

    Implement UnitOfWork Design Pattern

---
+++ Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+
+        // Generate Repository
+        IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>; 
+
+    }
+}
