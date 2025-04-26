7dcef0b2c797c6e55265505e1ef195e1b28a0d58
mnelshazly <mnelshazly7@gmail.com>
2025-04-26 07:33:34 +0300

    Implement NotFound EndPoint

--- Store.G03.Api/CustomMiddleWare/CustomExceptionHandlerMiddleWare.cs
+++ Store.G03.Api/CustomMiddleWare/CustomExceptionHandlerMiddleWare.cs
@@ -21,35 +21,56 @@ namespace Store.G03.Api.CustomMiddleWare
             try
             {
                 await _next.Invoke(context);
+
+                await HandleNotFoundEndPointAsync(context);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Something Went Wrong");
 
-                // Set Status Code For Response
+                await HandleExceptionAsync(context, ex);
+            }
+        }
 
-                //context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
-                //context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                
-                context.Response.StatusCode = ex switch
-                {
-                    NotFoundException => StatusCodes.Status404NotFound,
-                    _ => StatusCodes.Status500InternalServerError
-                };
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            // Set Status Code For Response
 
-                // Set Content Type For Response
-                //context.Response.ContentType = "application/json"; // No need to make this step when using WriteAsJsonAsync 
+            //context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+            //context.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
-                // Response Object
+            context.Response.StatusCode = ex switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            // Set Content Type For Response
+            //context.Response.ContentType = "application/json"; // No need to make this step when using WriteAsJsonAsync 
+
+            // Response Object
+            var response = new ErrorToReturn()
+            {
+                StatusCode = context.Response.StatusCode,
+                ErrorMessage = ex.Message
+            };
+
+            // Return Object As JSON
+            //var responseToReturn = JsonSerializer.Serialize(response);
+            //await context.Response.WriteAsync(responseToReturn);
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+
+        private static async Task HandleNotFoundEndPointAsync(HttpContext context)
+        {
+            if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+            {
                 var response = new ErrorToReturn()
                 {
-                    StatusCode = context.Response.StatusCode,
-                    ErrorMessage = ex.Message
+                    StatusCode = StatusCodes.Status404NotFound,
+                    ErrorMessage = $"Endpoint {context.Request.Path} is not found"
                 };
-
-                // Return Object As JSON
-                //var responseToReturn = JsonSerializer.Serialize(response);
-                //await context.Response.WriteAsync(responseToReturn);
 
                 await context.Response.WriteAsJsonAsync(response);
             }
