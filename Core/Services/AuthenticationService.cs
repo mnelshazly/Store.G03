64165b47484521492eea47b7300f364222fb8c22
mnelshazly <mnelshazly7@gmail.com>
2025-05-02 17:59:11 +0300

    Token Creation with JWT

--- Core/Services/AuthenticationService.cs
+++ Core/Services/AuthenticationService.cs
@@ -1,34 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Exceptions;
 using Domain.Models.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Services.Abstractions;
 using Shared.DataTransferObjects.IdentityModuleDtos;
 
 namespace Services
 {
-    public class AuthenticationService(UserManager<ApplicationUser> _userManager) : IAuthenticationService
+    public class AuthenticationService(UserManager<ApplicationUser> _userManager, IConfiguration _configuration) : IAuthenticationService
     {
         public async Task<UserDto> LoginAsync(LoginDto loginDto)
         {
             // Check if Email exists
-            var User = await _userManager.FindByEmailAsync(loginDto.Email);
-            if (User is null) throw new UserNotFoundException(loginDto.Email);
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user is null) throw new UserNotFoundException(loginDto.Email);
 
             // Check Password
-            var IsPasswordValid = await _userManager.CheckPasswordAsync(User, loginDto.Password);
+            var IsPasswordValid = await _userManager.CheckPasswordAsync(user, loginDto.Password);
             if (IsPasswordValid)
             {
                 // Return UserDto
                 return new UserDto()
                 {
-                    DisplayName = User.DisplayName,
-                    Email = User.Email,
-                    Token = "TOKEN"
+                    DisplayName = user.DisplayName,
+                    Email = user.Email,
+                    Token = await CreateTokenAsync(user)
                 };
 
             }
@@ -42,7 +46,7 @@ namespace Services
         public async Task<UserDto> RegisterAsync(RegisterDto registerDto)
         {
             // Mapping Register Dto => Application User
-            var User = new ApplicationUser()
+            var user = new ApplicationUser()
             {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
@@ -51,11 +55,11 @@ namespace Services
             };
 
             // Create User [Application User]
-            var result = await _userManager.CreateAsync(User, registerDto.Password);
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
 
             if (result.Succeeded)
             {
-                return new UserDto() { DisplayName = User.DisplayName, Email = User.Email, Token = "TOKEN" };
+                return new UserDto() { DisplayName = user.DisplayName, Email = user.Email, Token = await CreateTokenAsync(user) };
             }
             else
             {
@@ -65,6 +69,35 @@ namespace Services
 
             // Return UserDto
             // Throw Bad Request Exception
+        }
+
+        private async Task<string> CreateTokenAsync(ApplicationUser user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var secretKey = _configuration.GetSection("JWTOptions")["SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer : _configuration.GetSection("JWTOptions")["Issuer"],
+                audience: _configuration.GetSection("JWTOptions")["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token); 
         }
     }
 }
--- Core/Services/ServiceManager.cs
+++ Core/Services/ServiceManager.cs
@@ -7,17 +7,18 @@ using AutoMapper;
 using Domain.Contracts;
 using Domain.Models.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Services.Abstractions;
 
 namespace Services
 {
-    public class ServiceManager(IUnitOfWork unitOfWork, IMapper mapper, IBasketRepository basketRepository, UserManager<ApplicationUser> userManager) : IServiceManager
+    public class ServiceManager(IUnitOfWork unitOfWork, IMapper mapper, IBasketRepository basketRepository, UserManager<ApplicationUser> userManager, IConfiguration configuration) : IServiceManager
     {
         public IProductService ProductService { get; } = new ProductService(unitOfWork, mapper);
 
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, mapper));
         public IBasketService BasketService => _LazyBasketService.Value;
-        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager));
+        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager, configuration));
 
         public IAuthenticationService AuthenticationService => _LazyAuthenticationService.Value;
 
